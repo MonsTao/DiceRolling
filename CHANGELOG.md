2a56dd74503237f52f733dc3e398919d80092969
MonsTao <monstao@126.com>
2020-12-17 10:23:54 +0800

    更新至1.3.0版本

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,16 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+## [1.3.0] - 2020-12-09
+### Changed
+- 更改了随机数生成算法
+
+### Fixed
+- PairdVaryDiceRolling现在会正确地进行计算
+
+## [1.2.0] - 2020-12-09
+### Changed
+- 更改了文件路径
 
 ## [1.1.0] - 2020-12-09
 ### Added
---
+++ Runtime/Random.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace  AdvanceRandom
+{
+    public class Random
+    {
+        private static System.Random random = new System.Random();
+
+        private static double BoxMuller()
+        {
+                double v1, v2, s;
+                do
+                {
+                    v1 = 2.0 * random.NextDouble() - 1.0;
+                    v2 = 2.0 * random.NextDouble() - 1.0;
+                    s = v1 * v1 + v2 * v2;
+                } while (s >= 1.0 || s == 0);
+                s = System.Math.Sqrt((-2.0 * System.Math.Log(s)) / s);
+                return v1 * s;         
+        }
+
+        public static double BoxMuller(double mean, double standard_deviation)
+        {
+            return mean + BoxMuller() * standard_deviation;
+        }
+
+
+        public static  int GaussRandom(int min ,int max)
+        {
+           // Debug.Log((int)BoxMuller(min + (max - min) / 2.0, 1.0));
+            return (int)BoxMuller(min + (max - min) / 2.0, 1.0);
+        }
+        public static float GaussRandom(float min, float max)
+        {
+            return (float)BoxMuller(min + (max - min) / 2.0, 1.0);
+        }
+
+        public static int AverageRandom(int min, int max)
+        {
+            System.Random random = new System.Random();
+            //  Debug.Log(random.Next(min, max));
+            return random.Next(min, max);
+        }
+
+        public static float AverageRandom(float min, float max)
+        {
+            return UnityEngine.Random.Range(min, max);
+        }
+    }
+} 
+
